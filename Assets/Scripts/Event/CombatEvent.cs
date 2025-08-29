99b634ba509dd9faaf0706c01e685622d88408ab
Vinicius Camilotti <vinicamilotti@gmail.com>
2025-08-29 13:07:02 -0300

    Update CombatEvent.cs

--- Assets/Scripts/Event/CombatEvent.cs
+++ Assets/Scripts/Event/CombatEvent.cs
@@ -1,26 +1,86 @@
-using ARiskyGame.Types;
+﻿using ARiskyGame.Types;
+using System.Collections.Generic;
 using UnityEngine;
 
+public enum EnemyType : byte
+{
+    Pirate,
+    RogueIA,
+    AncientAlien,
+}
+
+public class Enemy
+{
+    public string Title;
+    public string Description;
+    public int CombatPower;
+}
 public class CombatEvent : ExpeditionEvent
 {
+    Dictionary<EnemyType, Enemy> enemies = new()
+    {
+        {
+            EnemyType.Pirate, new()
+            {
+                Title = "Combat: Alien Pirates",
+                Description = "Some wierd lookin green creatures with eye patches are attacking your ship",
+                CombatPower = Random.Range(2, 4)
+            }
+        },
+        {
+            EnemyType.RogueIA,
+            new()
+            {
+                Title = "Combat: Rogue IA",
+                Description = " 01010011 01101000 01101001 01110000 00100000 01100100 01100101 01110100 01100101 01100011 01110100 01100101 01100100 00101100 00100000 01000100 01000101 01010011 01010100 01010010 01001111 01011001 00100001",
+                CombatPower = Random.Range(3, 5)
+            }
+        },
+        {
+            EnemyType.AncientAlien,
+            new()
+            {
+                Title = "Combat: Ancient alien race",
+                Description = "B̝̪͈̪͙̱͐̀̌͐ͣ̑ͨ͐ͪͪ̕ͅe̸̸̶̵̷̛̛̤̪̯͇̖̯̤̪̯͙̯ͧͥ͒̅̌͋̋̄ͥ̈́͆͛̋̒ͣ̾ͥ̚͜͢͡ͅ_ a͓͉̗̿̂͘f̝̱͎̪͋̋ͩ́̃ͨ̾̏͗̈͛͛̿͑͜͞ͅf̣͈͈͕̀͊ͭ͝͝͠͝r̸̢̟̭͍͎̲͚̝̥̙̥͔̬͖͈̞̮̝̎͗͐̾͗̂ͪ̉ͫͮ̊͆͒ͭ̅̃̔ͭ̔̕̕͞͝ͅa̢͇̝̘͉̜̹̬͊̅ͩ̉͑ͯ̿͟͞͝i͇̟͒_̮͍̺͚̱̳̮̯͇͍̲̐̏̀̑̏̈́ͯ̐̂͂͐͘̕͘͜_̷̴̝̣̃̚̕d̴̛̛͉̲̟̳̎͂͆͂͘"
+                CombatPower = Random.Range(4, 6)
+            }
+       }
+    };
     int CombatPower = 2;
-
+    float combatOdds = 0;
     public CombatEvent() : base()
     {
     }
 
+    Enemy DeterminateEncouter()
+    {
+        EnemyType type = Expedition.ExpeditionController.GalaxyDepth switch
+        {
+            Assets.Types.GalaxyDepth.Depth1 => EnemyType.Pirate,
+            Assets.Types.GalaxyDepth.Depth2 => (EnemyType)Random.Range(0, 1),
+            Assets.Types.GalaxyDepth.Depth3 => (EnemyType)Random.Range(1, 2),
+            _ => EnemyType.Pirate
+        };
+
+        return enemies[type];
+    }
     public override void Init()
     {
+        Enemy getEnemy = DeterminateEncouter();
+        CombatPower = getEnemy.CombatPower;
+        var combatFactor = (Expedition.Player.CombatPower + CombatPower);
 
-        Title = "Teste combate";
+        combatOdds = (float)Expedition.Player.CombatPower / (float)combatFactor;
+
+        Title = getEnemy.Title;
         Steps.Enqueue(new()
         {
-            Description = "Somee pirates attack your ship",
+            Description = getEnemy.Description,
             StepAction = new()
             {
                 new()
                 {
-                    Description = "Attack",
+                    Description = $"Attack ({combatOdds * 100}%)",
                     StepAction = Fight
                 },
                 new()
@@ -29,24 +89,25 @@ public class CombatEvent : ExpeditionEvent
                     StepAction = Flee,
                 }
             }
-            
+
         });
     }
 
     void Fight()
     {
-        var odds = Expedition.Player.CombatPower / Expedition.Player.CombatPower + CombatPower;
 
-        if (Random.value < odds)
+        if (Random.value < combatOdds)
         {
-            Steps.Enqueue(new() {
+            Steps.Enqueue(new()
+            {
                 Description = "You win the fight!",
                 StepAction = new() { new() { StepAction = End, Description = "Voltar" } }
             });
         }
         else
         {
-            Steps.Enqueue(new() {
+            Steps.Enqueue(new()
+            {
                 Description = "You lose the fight!",
                 StepAction = new() { new() { StepAction = End, Description = "Voltar" } }
             });
@@ -57,7 +118,8 @@ public class CombatEvent : ExpeditionEvent
 
     void Flee()
     {
-        Steps.Enqueue(new() {
+        Steps.Enqueue(new()
+        {
             Description = "You flee",
             StepAction = new() { new() { StepAction = End, Description = "Voltar" } }
         });
@@ -65,8 +127,8 @@ public class CombatEvent : ExpeditionEvent
         NextStep();
     }
 
-    
 
-    
+
+
 
 }
